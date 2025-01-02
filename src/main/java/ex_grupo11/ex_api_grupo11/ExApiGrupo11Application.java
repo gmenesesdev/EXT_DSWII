f22bcf07b3a2a7ae34b745108f0cbae835cafac1
Gonzalo Meneses <gonzalo.menesesg@gmail.com>
2025-01-01 22:12:17 -0300

    First commit

---
+++ src/main/java/ex_grupo11/ex_api_grupo11/ExApiGrupo11Application.java
@@ -0,0 +1,13 @@
+package ex_grupo11.ex_api_grupo11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExApiGrupo11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExApiGrupo11Application.class, args);
+	}
+
+}
---
+++ src/test/java/ex_grupo11/ex_api_grupo11/ExApiGrupo11ApplicationTests.java
@@ -0,0 +1,13 @@
+package ex_grupo11.ex_api_grupo11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExApiGrupo11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
